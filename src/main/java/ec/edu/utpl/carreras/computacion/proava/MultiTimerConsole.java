3edc0f4f75a0ad3c923e744dda00c8003c2a38f4
Mauricio5c <taller@utpl.edu.ec>
2026-04-23 10:50:17 -0500

    Parte 2: TimerTask implementa Runnable

--- src/main/java/ec/edu/utpl/carreras/computacion/proava/MultiTimerConsole.java
+++ src/main/java/ec/edu/utpl/carreras/computacion/proava/MultiTimerConsole.java
@@ -1,12 +1,13 @@
 package ec.edu.utpl.carreras.computacion.proava;
 
-/**
- * Hello world!
- */
 public class MultiTimerConsole {
     public static void main(String[] args) {
         TimerTask timerA = new TimerTask("Temporizador A", 8);
-        timerA.run();
-        System.out.println();
+        TimerTask timerB = new TimerTask("Temporizador B", 5);
+        TimerTask timerC = new TimerTask("Temporizador C", 3);
+
+        new Thread(timerA, "Hilo-A").start();
+        new Thread(timerB, "Hilo-B").start();
+        new Thread(timerC, "Hilo-C").start();
     }
 }
